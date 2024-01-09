870de78f0465b1e7203dc5d1ec5979f703728c77
Mindy D Tran <mindytran98@gmail.com>
2024-01-09 15:06:40 -0500

    add post method

--- routes/comments.js
+++ routes/comments.js
@@ -4,9 +4,21 @@ const router = express.Router();
 const comments = [];
 
 router
- .route("/")
- .get((req, res) => {
-    res.json(comments)
- });
+  .route("/")
+  .get((req, res) => {
+    res.json(comments);
+  })
+  .post((req, res) => {
+    if (req.body.userId && req.body.postId && req.body.body) {
+      const comment = {
+        id: comments.length + 1,
+        userId: req.body.userId,
+        postId: req.body.postId,
+        body: req.body.body,
+      };
+      comments.push(comment);
+      res.json(comments[comment.length - 1]);
+    } else res.json({ error: "Insufficient data" });
+  });
 
 module.exports = router;
